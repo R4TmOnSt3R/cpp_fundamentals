f4b7a315f6c9abe93e482150842cf743284103af
Mohammad-Rakibuzzaman <169205404+R4TmOnSt3R@users.noreply.github.com>
2025-05-26 07:07:09 +0600

    add todays work

---
+++ 01basic-concepts/vscode-template-main/main.cpp
@@ -0,0 +1,32 @@
+/*
+* above printed details of the user.
+* taken 2 random number from user and print the sum of those values.sum
+* by Rat
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+      //author details
+      printf("hello from Ratul! \n");
+      printf("hello from mirpur Dhaka \n");
+      printf("My favorite hobby is Travelling and photoshoot! \n");
+      printf("By RaT \n");
+
+      //declaring variables
+      // int x, y;
+      //another variable to store sum
+      // int sum;
+      // cout << "Type a number: ";
+      // cin >> x;
+      // cout << "Type another number: ";
+      // cin >> y;
+      // sum = x + y;
+      // cout << "Sum is: " << sum;
+
+      return 0;
+}
