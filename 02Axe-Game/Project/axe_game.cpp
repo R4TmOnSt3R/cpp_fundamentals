8628a75db44a3b8bfcc6c319f6c96982c6ff2b92
Mohammad-Rakibuzzaman <169205404+R4TmOnSt3R@users.noreply.github.com>
2025-05-30 11:45:52 +0600

    push file

--- 02Axe-Game/Project/axe_game.cpp
+++
@@ -1,7 +0,0 @@
-#include "raylib.h"
-
-int main() 
-{
-      InitWindow(300, 200, "ratuls home");
-
-}
---
+++ 02Axe-Game/Project/main.cpp
@@ -0,0 +1,20 @@
+#include "raylib.h"
+int main()
+{
+
+    float root_beer;
+    root_beer = 1.99;
+    double cheese_burger;
+    cheese_burger = 5.99;
+    bool shouldHaveLunch;
+    shouldHaveLunch = true;
+
+    int width;
+    width = 1200;
+
+    // InitWindow(width, 700, "ratuls home");
+
+    int some_variable = 0;
+
+    return 0;
+}
