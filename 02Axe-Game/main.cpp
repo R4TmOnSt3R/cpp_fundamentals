1e9adcacdbdebfd7d8380131e0b981120868c5cd
Mohammad-Rakibuzzaman <169205404+R4TmOnSt3R@users.noreply.github.com>
2025-05-31 09:27:40 +0600

    alright push till 08 tut

--- 02Axe-Game/main.cpp
+++ 02Axe-Game/main.cpp
@@ -2,13 +2,36 @@
 int main()
 {
 
-    float root_beer = 1.99;
-    double cheese_burger{5.99};   
-    bool shouldHaveLunch{};
+    // //Comparison Operators
+    // bool equal{ 4 == 9 }; //false
+ 
+    // bool notEqual{ 4 != 9 }; //true
+    
+    // bool less{ 4 < 9 }; //true
+    
+    // bool greater{ 4 > 9 }; //false
+    
+    // bool lessEqual{ 4 <= 9 }; //true
+    
+    // bool greaterEqual{ 4 >= 9 }; //false
+
+
+
+    // float root_beer = 1.99;
+    // double cheese_burger{5.99};   
+    // bool shouldHaveLunch{};
 
     int width;
-    width = 1200;
-    InitWindow(width, 700, "ratuls home");
+    width = 350;
+    InitWindow(width, 200, "ratuls home"); 
+
+    while (WindowShouldClose() != true) // this WindowShouldClose return a bool value
+    {
+        BeginDrawing();
+        ClearBackground(RED);
+        EndDrawing();
+    }
+    
 
 //     int some_variable = 0;
 
