7cbcf064597cfda358756df1b04255fb9c8769b3
Mohammad-Rakibuzzaman <169205404+R4TmOnSt3R@users.noreply.github.com>
2025-06-01 11:15:25 +0600

    push till v13

--- 02Axe-Game/main.cpp
+++ 02Axe-Game/main.cpp
@@ -21,14 +21,54 @@ int main()
     // double cheese_burger{5.99};   
     // bool shouldHaveLunch{};
 
-    int width;
-    width = 350;
-    InitWindow(width, 200, "ratuls home"); 
+    //window dimensions
+    int width{850};
+    int height{500};
+    InitWindow(width, height, "ratuls home"); 
+
+    //circle coordinates
+    int circle_x{220};
+    int circle_y{220};
+
+    //rectangle coordinates
+    int axe_X{405};
+    int axe_Y{0};
+
+    int direction{10};
+
+    //forcing down to window frame 
+    SetTargetFPS(60);
 
     while (WindowShouldClose() != true) // this WindowShouldClose return a bool value
     {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
+
+        //game logic starts
+
+
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+        DrawRectangle(axe_X, axe_Y, 50, 50, RED);
+
+        //move the axe
+        axe_Y += direction;
+        if(axe_Y > 450 || axe_Y < 0)
+        {
+            direction = -direction;
+        }
+
+        if (IsKeyDown(KEY_D) && circle_x < 835)
+        {
+            circle_x +=  10;
+        }
+        if (IsKeyDown(KEY_A) && circle_x > 15)
+        {
+            circle_x -= 10;
+        }
+
+        
+
+        //game logic ends
         EndDrawing();
     }
     
