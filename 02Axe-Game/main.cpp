6d473ea8345eb27b769c400d404fcb42f006d447
sloppyG33K <aimeeclark726@gmail.com>
2025-06-03 16:31:41 +0600

    add logic for edge detect

--- 02Axe-Game/main.cpp
+++ 02Axe-Game/main.cpp
@@ -22,17 +22,36 @@ int main()
     // bool shouldHaveLunch{};
 
     //window dimensions
-    int width{850};
-    int height{500};
+    int width{800};
+    int height{450};
     InitWindow(width, height, "ratuls home"); 
 
     //circle coordinates
-    int circle_x{220};
-    int circle_y{220};
+    int circle_x{200};
+    int circle_y{200};
+
+    int circle_radius{25};
+
+    //circle edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
+
 
     //rectangle coordinates
-    int axe_X{405};
-    int axe_Y{0};
+    int axe_x{400};
+    int axe_y{0};
+
+    int axe_length{50};
+
+    //axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_length};
+
+
 
     int direction{10};
 
@@ -47,21 +66,21 @@ int main()
         //game logic starts
 
 
-        DrawCircle(circle_x, circle_y, 25, BLUE);
-        DrawRectangle(axe_X, axe_Y, 50, 50, RED);
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
         //move the axe
-        axe_Y += direction;
-        if(axe_Y > 450 || axe_Y < 0)
+        axe_y += direction;
+        if(axe_y > height || axe_y < 0) 
         {
             direction = -direction;
         }
 
-        if (IsKeyDown(KEY_D) && circle_x < 835)
+        if (IsKeyDown(KEY_D) && circle_x < width)
         {
             circle_x +=  10;
         }
-        if (IsKeyDown(KEY_A) && circle_x > 15)
+        if (IsKeyDown(KEY_A) && circle_x > 0)
         {
             circle_x -= 10;
         }
